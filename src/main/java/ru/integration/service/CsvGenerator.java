868ccccaf934ef8118ef186c50bf10cede01eaa5
unknown <dmitriy.fedorov.1991@list.ru>
2025-11-25 14:55:09 +0500

    fix date format

--- src/main/java/ru/integration/service/CsvGenerator.java
+++ src/main/java/ru/integration/service/CsvGenerator.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 public class CsvGenerator implements Processor {
     private static final Logger logger = LoggerFactory.getLogger(CsvGenerator.class);
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
     
     @Override
     public void process(Exchange exchange) throws Exception {
